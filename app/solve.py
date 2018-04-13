3aca9011838bee8555be50c607cdc92fc40084fd
Matteo Vitali <trottomv@gmail.com>
2018-04-13 11:03:01 +0200

    clean code for generator and solve

--- app/solve.py
+++ app/solve.py
@@ -5,7 +5,6 @@ import time
 class Maze:
     MOVESARR = []
     SOLARR = []
-    TMPARR = []
 
     with open('txt/maze.txt', 'r') as f:
         lines = (line.rstrip("\r\n") for line in f)
@@ -30,31 +29,32 @@ class Maze:
         for y in range(len(maze)):
             for x in range(len(maze[y])):
                 G.add_node((y,x))
-                if y >0:
+                if y > 0:
                     G.add_edge((y-1,x),(y,x))
+                    # G.add_edge((y,x-1),(y,x))
+                if x > 0:
                     G.add_edge((y,x-1),(y,x))
-                if x >0:
-                    G.add_edge((y,x-1),(y,x))
-                    G.add_edge((y-1,x),(y,x))
+                    # G.add_edge((y-1,x),(y,x))
 
         for y in range(len(maze)):
             for x in range(len(maze[y])):
-                if maze[y][x]=='X':
+                if maze[y][x] == 'X':
                     G.remove_node((y,x))
 
         try:
-            TMPARR = []
+            MOVESARR = []
             for point in nx.algorithms.astar_path(G,(1,0),(len(maze)-2,len(maze[0])-1)):
-                Maze.MOVESARR.append(point)
-                Maze.TMPARR.append(point)
+                MOVESARR.append(point)
+                # Maze.TMPARR.append(point)
                 mazesolve[point[0]][point[1]] = '+'
                 if point == (1,0):
                     mazesolve[point[0]][point[1]] = 'S'
                 elif point == (len(maze)-2,len(maze[0])-1):
                     mazesolve[point[0]][point[1]] = 'E'
 
-            Maze.SOLARR.append(Maze.MOVESARR)
-            Maze.SOLARR.append(Maze.TMPARR)
+            # Maze.SOLARR.append(Maze.TMPARR)
+            Maze.SOLARR.append(MOVESARR)
+            Maze.MOVESARR.append(MOVESARR)
 
             open("txt/mazesolve.txt", "w").close()
             for i in range (len(mazesolve)):
@@ -69,12 +69,13 @@ class Maze:
 
     def output():
         print("solving... (Teseo is running away from Minotaur)")
+        print("")
         time.sleep(2)
         f = open("txt/mazesolve.txt", "r")
         print(f.read())
         f.close
 
-        print("Teseo is save in", len(Maze.SOLARR[0])-1, "moves")
+        print("Teseo is save in", len(Maze.MOVESARR[0])-1, "moves")
 
     def nosoloutput():
         print("solving... (Teseo is running away from Minotaur)")
@@ -82,102 +83,11 @@ class Maze:
         print("There is no solution for Teseo... sorry, try again!")
         print("Try to generate a new maze")
 
-    # def  countsol():
-    #     with open('txt/mazesolve.txt', 'r') as f:
-    #         lines = (line.rstrip("\r\n") for line in f)
-    #         maze  = np.array([list(line) for line in lines])
-    #         # mazesolve = np.asarray(maze)
-    #
-    #     G = nx.Graph()
-    #     for y in range(len(maze)):
-    #         for x in range(len(maze[y])):
-    #             G.add_node((y,x))
-    #             if y >0:
-    #                 G.add_edge((y-1,x),(y,x))
-    #             if x >0:
-    #                 G.add_edge((y,x),(y,x-1))
-    #
-    #     for y in range(len(maze)):
-    #         for x in range(len(maze)):
-    #             if maze[y][x]=='X':
-    #                 G.remove_node((y,x))
-    #                 if maze[y][x]=='+':
-    #                     G.remove_node((y,x))
-    #
-    #     # try:
-    #     TMPARR = []
-    #     for point in nx.algorithms.astar_path(G,(1,0),(len(maze)-2,len(maze[0])-1)):
-    #         Maze.MOVESARR.append(point)
-    #         Maze.TMPARR.append(point)
-    #         mazesolve[point[0]][point[1]] = '+'
-    #         if point == (1,0):
-    #             mazesolve[point[0]][point[1]] = 'S'
-    #         elif point == (len(maze)-2,len(maze[0])-1):
-    #             mazesolve[point[0]][point[1]] = 'E'
-    #
-    #     Maze.SOLARR.append(Maze.MOVESARR)
-    #     Maze.SOLARR.append(Maze.TMPARR)
-    #
-    #     open("txt/mazesolve.txt", "w").close()
-    #     for i in range (len(mazesolve)):
-    #         text_file = open("txt/mazesolve.txt", "a")
-    #         text_file.write("".join(mazesolve[i])+ '\n')
-    #         text_file.close()
-    #
-    #     return True
-    #     ######################3
-    #         #         for i in range (len(Maze.SOLARR)):
-    #         #             try:
-    #         #                 Maze.TMPARR = []
-    #         #                 for point in nx.algorithms.astar_path(Maze.G,(1,0),(len(Maze.maze)-2,len(Maze.maze[0])-1)):
-    #         #                     Maze.TMPARR.append(point)
-    #         #             except:
-    #         #                     Maze.G.add_node((Maze.SOLARR[i][j][0],Maze.SOLARR[i][j][1]))
-    #         #
-    #         #                 #     pass
-    #         #
-    #         #             for i in range(len(Maze.SOLARR)):
-    #         #                 if Maze.TMPARR != Maze.SOLARR[i]:
-    #         #                     Maze.SOLARR.append(Maze.TMPARR)
-    #         #                     # print(len(Maze.SOLARR))
-    #         # print(Maze.SOLARR)
-    #         #                 # print(Maze.SOLARR[0:4])
-    #         #
-    #         # print("(There where", len(Maze.SOLARR), "possible solutions for Teseo)")
-    #         # # print(Maze.SOLARR)
-    #
-    #     # except:
-    #     #     for i in range (len(Maze.SOLARR)):
-    #     #         for j in range (len(Maze.SOLARR[i])): #Maze.SOLARR[i-1], Maze.SOLARR[-1]):
-    #     #             if maze[i][j] == '+':
-    #     #                 Maze.G.add_node((i,j))
-    #     #                 Maze.G.add_node((Maze.SOLARR[i][0],Maze.SOLARR[i][1]))
-    #     #                 # try:
-    #     #                 Maze.TMPARR = []
-    #     #                 for point in nx.algorithms.astar_path(Maze.G,(1,0),(len(Maze.maze)-2,len(Maze.maze[0])-1)):
-    #     #                     Maze.TMPARR.append(point)
-    #     #         for i in range(len(Maze.SOLARR)):
-    #     #             if Maze.TMPARR != Maze.SOLARR[i]:
-    #     #                 Maze.SOLARR.append(Maze.TMPARR)
-    #     #     pass
-    #     #     # Maze.countsol()
-
-
-
 def main():
-    # for y in range(len(maze)):
-    #     for x in range(len(maze)):
     if Maze.solve() is False:
         Maze.nosoloutput()
     else:
         Maze.output()
-        # Maze.countsol()
-        pass
-    # if Maze.solve() is False:
-    #     Maze.nosoloutput()
-    # else:
-    #     Maze.output()
-    #     pass
 
 if __name__=="__main__":
     main()
